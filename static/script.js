db7181353d99a0635a52a9e2a186b354845dd70a
thgank <sultanhuman02@gmail.com>
2023-11-11 02:05:17 +0600

    finish ver

--- static/script.js
+++ static/script.js
@@ -1,41 +1,22 @@
-function toggleNewsBody(index) {
-    var newsBody = document.getElementById('news-body-' + index); // get news body element by id
-    if (newsBody.style.maxHeight) { // check if it's visible
-        newsBody.style.maxHeight = null; // if yes, set max height to null and hide it
-    } else {
-        newsBody.style.maxHeight = newsBody.scrollHeight + "px"; // if no, show it completely by setting max to scroll height
-    }
-}
 
 function applyFilters() {
     var titleSearchTerm = document.getElementById("search-title").value.toLowerCase();
-    var bodySearchTerm = document.getElementById("search-body").value.toLowerCase(); // formatting in lower case to search
+    var bodySearchTerm = document.getElementById("search-body").value.toLowerCase();
     var startDate = document.getElementById("start-date").value;
     var endDate = document.getElementById("end-date").value;
-
-    // catch for case when end-date is not entered, then it will be today's date
-    if (!endDate) {
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); // january - 0
-        var yyyy = today.getFullYear();
-
-        endDate = yyyy + '-' + mm + '-' + dd;
-    }
 
     var newsItems = document.getElementsByClassName("news-item");
 
     for (var i = 0; i < newsItems.length; i++) {
         var newsItem = newsItems[i];
         var title = newsItem.getAttribute("data-title").toLowerCase();
-        var body = newsItem.getAttribute("data-body").toLowerCase(); // formatting json values to lowercase to search
+        var body = newsItem.getAttribute("data-body").toLowerCase();
         var createdDate = new Date(newsItem.getAttribute("data-created-date"));
 
         var matchesTitle = title.includes(titleSearchTerm);
         var matchesBody = body.includes(bodySearchTerm);
         var isInRange = isDateInRange(createdDate, startDate, endDate);
-        
-        //check if data matches entered text
+
         if ((matchesTitle || titleSearchTerm === '') && (matchesBody || bodySearchTerm === '') && isInRange) {
             newsItem.style.display = "block";
         } else {
@@ -43,6 +24,7 @@ function applyFilters() {
         }
     }
 }
+
 
 function isDateInRange(date, startDate, endDate) {
     if (!startDate && !endDate) { // if no start date and end date are provided, the date is considered in range
